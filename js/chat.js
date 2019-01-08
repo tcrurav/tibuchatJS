d844b57a6356d205822b0dc9c5b0ff1606f76b12
tcrurav <tiburcio.cruz@gmail.com>
2019-01-08 11:25:41 +0000

    just a test for today's class

---
+++ js/chat.js
@@ -0,0 +1,33 @@
+window.onload = initialize;
+
+var db;
+
+function initialize(){
+    db = firebase.database();
+    showAllMessages();
+
+    document.getElementById("form-message").addEventListener("submit", sendMessage);
+}
+
+function sendMessage(e){
+    e.preventDefault();
+
+    let refMessages = db.ref("messages");
+    refMessages.push({
+        name: e.target.name.value,
+        message: e.target.message.value
+    });
+}
+
+function showAllMessages(){
+    let refMessages = db.ref("messages");
+
+    refMessages.on("value", snap => {
+        let data = snap.val();
+        let allMessages = "";
+        for (var key in data){
+            allMessages += data[key].name + ": " + data[key].message + "</br>";
+        }
+        document.getElementById("all-messages").innerHTML = allMessages;
+    });
+}
